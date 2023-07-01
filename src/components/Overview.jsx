ebadf3814cc483549db78b8b0c6acbe0d3a9eecd
vvhybe <vvhybe@hotmail.com>
2023-07-01 20:46:57 +0100

    Envirment overview

---
+++ src/components/Overview.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect, useRef } from "react"
+import { OverviewStockImgs } from "../utils"
+// import "handyscript/lib/math";
+
+export default function Overview() {
+    const [classesOverview, setClassesOverview] = useState(OverviewStockImgs.classes[0]);
+    const [studioOverview, setStudioOverview] = useState(OverviewStockImgs.studio[0]);
+    const [yogaOverview, setYogaOverview] = useState(OverviewStockImgs.yoga[0]);
+
+    const [animateTrigger, setAnimateTrigger] = useState(false);
+
+    const img = useRef(0);
+
+    useEffect(() => {
+        // every 3s show a new image (looping)
+        let overviewImgs = setInterval(() => {
+            setAnimateTrigger(false);
+
+            setTimeout(() => {
+                img.current = (img.current + 1) % 5;
+                setAnimateTrigger(true);
+                setClassesOverview(OverviewStockImgs.classes[img.current]);
+                setStudioOverview(OverviewStockImgs.studio[img.current]);
+                setYogaOverview(OverviewStockImgs.yoga[img.current]);
+            }, 500);
+                
+        }, 3000);
+
+        return () => clearInterval(overviewImgs);
+    });
+
+  return (
+    <section className="container w-full px-4 py-10 flex flex-1 flex-col sm:flex-row justify-evenly bg-yoga-white gap-4 sm:gap-8">
+        <div className="h-full flex flex-col items-center justify-between sm:gap-4">
+            <img  src={classesOverview} className={`h-[400px] w-[400px] aspect-square object-contain select-none transition-opacity duration-500 ${animateTrigger ? 'opacity-100' : 'opacity-0'}`} alt="Yoga Classes Gallery Images" />
+            <h3 className="cinzel text-2xl font-bold uppercase">Classes</h3>
+        </div>
+        <div className="h-full flex flex-col items-center justify-between sm:gap-4">
+            <img src={studioOverview} className={`h-[400px] w-[400px] aspect-square object-contain select-none transition-opacity duration-500 ${animateTrigger ? 'opacity-100' : 'opacity-0'}`} alt="Yoga Studio Gallery Images" />
+            <h3 className="cinzel text-2xl font-bold uppercase">Studio</h3>
+        </div>
+        <div className="h-full flex flex-col items-center justify-between sm:gap-4">
+            <img src={yogaOverview} className={`h-[400px] w-[400px] aspect-square object-contain select-none transition-opacity duration-500 ${animateTrigger ? 'opacity-100' : 'opacity-0'}`} alt="Yoga Pose Gallery Images" />
+            <h3 className="cinzel text-2xl font-bold uppercase">Yoga</h3>
+        </div>
+    </section>
+  )
+}
