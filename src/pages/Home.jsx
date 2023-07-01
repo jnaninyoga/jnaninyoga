fb961a0b78c7a6a4c03f95e89a0f7371a5d6a41d
vvhybe <vvhybe@hotmail.com>
2023-07-01 20:51:00 +0100

    display the Home components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import AboutMe from "../components/AboutMe";
+import ContactCard from "../components/ContactCard";
+import Hero from "../components/Hero";
+import Overview from "../components/Overview";
+import Yoga from "../components/Yoga";
 import Footer from "../layouts/Footer";
 import Header from "../layouts/Header";
 
@@ -5,6 +10,11 @@ export default function Home() {
   return (
     <>
     <Header/>
+    <Hero/>
+    <Overview/>
+    <Yoga/>
+    <ContactCard/>
+    <AboutMe/>
     <Footer/>
     </>
   )
