6cd55f5db205420175426ce3e9c2dd084a751918
vvhybe <vvhybe@hotmail.com>
2023-07-03 15:48:35 +0100

    Custom AOS animation using Intersect Observer

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,22 +1,32 @@
+import { useRef } from "react";
+import { useIntersectView } from "../hooks";
 import { Link } from "react-router-dom";
 import aboutme from "../assets/imgs/stock/aboutme.png";
 import GreenMat from "../assets/imgs/spine/GreenMat.png";
 import LotusOverlay from "../assets/imgs/icons/lotusOverlay.png";
 
 export default function AboutMe() {
+  const aboutmeRef = useRef(null);
+  const aboutWrapper = useRef(null);
+  const btn = useRef(null);
+
+  const isAboutMeIntersected = useIntersectView(aboutmeRef);
+  const isAboutWrapperIntersected = useIntersectView(aboutWrapper);
+  const isBtnIntersected = useIntersectView(btn);
+
   return (
-    <section id="aboutme" className="container sm:pb-0 pb-4 sm:h-[540px] flex flex-1 justify-center items-start text-yoga-white  sm:mt-24 sm:flex-row flex-col" style={{backgroundImage: `url(${GreenMat})`}}>
-        <img src={aboutme} className="sm:w-1/3 w-full h-full object-cover object-center" alt="Yoga Coash Profile picture" />
-        <div className="relative sm:pl-8 sm:py-12 py-8 h-full flex flex-1 justify-start sm:items-start items-center flex-col gap-10">
-            <img src={LotusOverlay} className="absolute sm:bottom-6 bottom-0 sm:right-4 right-1 object-cover object-center mix-blend-screen" alt="Lotus Overlay" />
-            <h2 className="cinzel text-3xl uppercase font-bold text-yoga-white">About Me</h2>
-            <p className="sm:pl-6 text-yoga-white text-lg text-justify w-[90%] z-20">
+    <section ref={aboutmeRef} id="aboutme" className="container sm:pb-0 pb-4 sm:h-[540px] flex flex-1 justify-center items-start text-yoga-white  sm:mt-24 sm:flex-row flex-col overflow-hidden" style={{backgroundImage: `url(${GreenMat})`}}>
+        <img src={aboutme} className={`${isAboutMeIntersected ? "sm:translate-x-0 opacity-100" : "sm:translate-x-[-100%] opacity-0"} sm:w-1/3 w-full h-full object-cover object-center transition-all duration-700`} alt="Yoga Coash Profile picture" />
+        <div ref={aboutWrapper} className="relative sm:pl-8 sm:py-12 py-8 h-full flex flex-1 justify-start sm:items-start items-center flex-col gap-10">
+            <img src={LotusOverlay} className={`${isBtnIntersected ? "opacity-100" : "opacity-0"} absolute sm:bottom-6 bottom-0 sm:right-4 right-1 object-cover object-center mix-blend-screen transition-all duration-700 delay-300`} alt="Lotus Overlay" />
+            <h2 className={`${isAboutWrapperIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} cinzel text-3xl uppercase font-bold text-yoga-white transition-all duration-500`}>About Me</h2>
+            <p className={`${isAboutWrapperIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} sm:pl-6 text-yoga-white sm:text-lg text-justify w-[90%] z-20 transition-all duration-500 delay-100`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eveniet voluptatem eos quae aspernatur praesentium et quas perferendis accusamus, voluptates dolore harum maiores nesciunt saepe, officia tempora quidem ducimus beatae.
                 Suscipit sequi natus modi, odit debitis excepturi delectus eaque perferendis! Deleniti vel placeat quibusdam architecto velit! Repellat esse quibusdam ipsam tempora dolor dolorum necessitatibus et. Mollitia ab quas laudantium incidunt.
                 Facilis exercitationem deserunt similique placeat at, eveniet obcaecati quidem perspiciatis veniam reprehenderit nulla perferendis ab vero eligendi soluta nisi corporis aperiam beatae ex. Vitae, eligendi quisquam deleniti deserunt quas quis?
                 Non, animi quam architecto obcaecati rem enim, distinctio cupiditate vitae dolor repudiandae iure debitis optio accusantium fugit dolorem voluptatibus sapiente aliquid eligendi. Dicta, sequi eveniet. Impedit quod alias amet veritatis? 
             </p>
-            <Link to={"/about"} className="yoga-btn text-yoga-black z-20">Learn More</Link>
+            <Link ref={btn} to={"/about"} title="Learn More" className={`${isBtnIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} yoga-btn text-yoga-black z-20 transition-all duration-500 delay-200`}>Learn More</Link>
         </div>
     </section>
   )
--- src/layouts/Card.jsx
+++ src/layouts/Card.jsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
 import icon from '../assets/imgs/icons/lotus.png';
+import PropTypes from 'prop-types';
+import { useRef } from "react";
+import { useIntersectView } from "../hooks";
+
+Card.propTypes = {
+  text: PropTypes.string,
+  twStyle: PropTypes.string,
+  title: PropTypes.string,
+  btn: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.string
+  ]),
+  linkTo: PropTypes.string
+};
 
 export default function Card({text="", twStyle="", title="", btn=false, linkTo="/"}) {
+  const card = useRef(null);
+  const isCardIntersected = useIntersectView(card);
+
   return (
-    <div className={`py-6 px-6 w-[90%] flex items-center justify-between flex-col gap-4 bg-yoga-white texture-v sm:texture-h texture-opacity-80 ${twStyle}`}>
-        <div className='h-12 w-12 bg-center bg-cover' style={{backgroundImage: `url(${icon})`}}></div>
-        {title ? <h3 className="cinzel text-xl sm:text-2xl font-bold uppercase text-center">{title}</h3> : null } 
-        <p className='cinzel text-center pb-2 sm:text-xl'>{text}</p>
-        {btn ? <Link to={linkTo} className="yoga-btn">{btn}</Link> : null}
+    <div ref={card} className={`py-6 px-6 w-[90%] flex items-center justify-between flex-col gap-4 bg-yoga-white texture-v sm:texture-h texture-opacity-80 ${twStyle} overflow-hidden`}>
+        <img src={icon} className={`${isCardIntersected ? "scale-1" : "scale-0"} h-12 w-12 transition-all duration-500 select-none`} alt="Red/pink lotus icon" />
+        {title ? <h3 className={`${isCardIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} cinzel text-xl sm:text-2xl font-bold uppercase text-center transition-all duration-500`}>{title}</h3> : null } 
+        <p className={`${isCardIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} cinzel text-center pb-2 sm:text-xl transition-all duration-500`}>{text}</p>
+        {btn ? <Link to={linkTo} title={btn} className={`${isCardIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} yoga-btn transition-all duration-500 delay-100`}>{btn}</Link> : null}
     </div>
   )
 }
