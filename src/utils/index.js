afa77d97f2115a22e65a9b984ac5d736eacd99ea
vvhybe <vvhybe@hotmail.com>
2023-07-10 22:31:24 +0100

    yoga types and switshing imports to webp

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,21 @@
 // Classes - Stock Images
-import C1 from "../assets/imgs/stock/classes-1.jpg";
-import C2 from "../assets/imgs/stock/classes-2.png";
-import C3 from "../assets/imgs/stock/classes-3.jpg";
-import C4 from "../assets/imgs/stock/classes-4.jpg";
-import C5 from "../assets/imgs/stock/classes-5.jpg";
+import C1 from "../assets/imgs/stock/classes-1.webp";
+import C2 from "../assets/imgs/stock/classes-2.webp";
+import C3 from "../assets/imgs/stock/classes-3.webp";
+import C4 from "../assets/imgs/stock/classes-4.webp";
+import C5 from "../assets/imgs/stock/classes-5.webp";
 // Studio - Stock Images
-import S1 from "../assets/imgs/stock/studio-1.jpg";
-import S2 from "../assets/imgs/stock/studio-2.jpg";
-import S3 from "../assets/imgs/stock/studio-3.png";
-import S4 from "../assets/imgs/stock/studio-4.jpg";
-import S5 from "../assets/imgs/stock/studio-5.jpg";
+import S1 from "../assets/imgs/stock/studio-1.webp";
+import S2 from "../assets/imgs/stock/studio-2.webp";
+import S3 from "../assets/imgs/stock/studio-3.webp";
+import S4 from "../assets/imgs/stock/studio-4.webp";
+import S5 from "../assets/imgs/stock/studio-5.webp";
 // Yoga - Stock Images
-import Y1 from "../assets/imgs/stock/yoga-1.jpg";
-import Y2 from "../assets/imgs/stock/yoga-2.jpg";
-import Y3 from "../assets/imgs/stock/yoga-3.jpg";
-import Y4 from "../assets/imgs/stock/yoga-4.jpg";
-import Y5 from "../assets/imgs/stock/yoga-5.png";
+import Y1 from "../assets/imgs/stock/yoga-1.webp";
+import Y2 from "../assets/imgs/stock/yoga-2.webp";
+import Y3 from "../assets/imgs/stock/yoga-3.webp";
+import Y4 from "../assets/imgs/stock/yoga-4.webp";
+import Y5 from "../assets/imgs/stock/yoga-5.webp";
 
 export const OverviewStockImgs = {
     classes: [C1, C2, C3, C4, C5],
@@ -31,6 +31,18 @@ export const formFields = [
     {type: 'textarea', name: 'message', placeholder: 'Message'}
 ];
 
+// Yoga Calander:
+import YT1 from "../assets/imgs/icons/yoga-type-1.webp";
+import YT2 from "../assets/imgs/icons/yoga-type-2.webp";
+import YT3 from "../assets/imgs/icons/yoga-type-3.webp";
+import YT4 from "../assets/imgs/icons/yoga-type-4.webp";
+
+export const yogaTypes = [
+    {name: 'Collective', img: YT1, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+    {name: 'Private', img: YT2, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+    {name: 'Special', img: YT3, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+    {name: 'Online', img: YT4, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+];
 
 export function copyright(hostname=window.location.hostname){
     return `Copyright © ${new Date().getFullYear()} ${hostname}, All Rights Reserved`
