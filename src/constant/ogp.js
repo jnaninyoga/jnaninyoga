a1babf2c5e3cb324837e3a4d0b9b9a1f5158f539
vvhybe <vvhybe@hotmail.com>
2023-07-14 17:55:05 +0100

    pages open graph protocol

---
+++ src/constant/ogp.js
@@ -0,0 +1,30 @@
+// OGP images
+import home from "../assets/imgs/OGP/Home.jpg";
+import about from "../assets/imgs/OGP/About.jpg";
+import contact from "../assets/imgs/OGP/Contact.jpg";
+import classes from "../assets/imgs/OGP/Classes.jpg";
+
+export default {
+    home:{
+        image: home,
+        url: "https://jnaninyoga.com",
+        type: "website"
+    },
+    about:{
+        image: about,
+        url: "https://jnaninyoga.com/about",
+        type: "website"
+    },
+    contact:{
+        image: contact,
+        url: "https://jnaninyoga.com/contact",
+        type: "website"
+    },
+    classes:{
+        image: classes,
+        url: "https://jnaninyoga.com/classes",
+        type: "website"
+    },
+}
+
+export const HostName = "https://jnaninyoga.com";
