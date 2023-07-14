da0387561ea0b28ba47b5e0abc097a143e23fa96
vvhybe <vvhybe@hotmail.com>
2023-07-14 17:56:23 +0100

    implement Suspense/Loader layout

--- src/layouts/Suspens.jsx
+++ src/layouts/Suspens.jsx
@@ -1,6 +1,11 @@
+import OverLaped from "./OverLaped"
 
 export default function Suspens() {
   return (
-    <div>Suspens</div>
+    <OverLaped>
+      <section className="w-[90vw] flex flex-1 items-center justify-center">
+        <h1 className="cinzel p-20 sm:text-8xl text-4xl text-yoga-white animate-pulse">Loading...</h1>
+      </section>
+    </OverLaped>
   )
 }
