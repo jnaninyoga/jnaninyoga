1c40deed4664e19eda0c44db526351a57e19eb3e
vvhybe <vvhybe@hotmail.com>
2023-07-14 01:37:35 +0100

    trnslatio of components using i18next

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,8 +1,14 @@
-import { useState, useEffect, useRef } from "react"
-import { OverviewStockImgs } from "../utils"
-// import "handyscript/lib/math";
+import { useState, useRef, useEffect } from "react"
+import { OverviewStockImgs, activePage } from "../utils"
+import { useTranslation } from "react-i18next";
 
 export default function Overview() {
+    const { t } = useTranslation();
+    const overview = t(`${activePage()}.overview`, { returnObjects: true });
+    const standardTitles = ["Classes","Studio","Yoga"];
+
+    const TTitles = () => Array.isArray(overview) ? overview : standardTitles;
+
     const [classesOverview, setClassesOverview] = useState(OverviewStockImgs.classes[0]);
     const [studioOverview, setStudioOverview] = useState(OverviewStockImgs.studio[0]);
     const [yogaOverview, setYogaOverview] = useState(OverviewStockImgs.yoga[0]);
@@ -33,15 +39,15 @@ export default function Overview() {
     <section className="container w-full px-4 py-10 flex flex-1 flex-col sm:flex-row justify-evenly bg-yoga-white gap-4 sm:gap-8">
         <div className="h-full flex flex-col items-center justify-between sm:gap-4">
             <img  src={classesOverview} className={`h-[400px] w-[400px] aspect-square object-contain select-none transition-opacity duration-500 ${animateTrigger ? 'opacity-100' : 'opacity-0'}`} alt="Yoga Classes Gallery Images" />
-            <h3 className="cinzel text-2xl font-bold uppercase">Classes</h3>
+            <h3 className="cinzel text-2xl font-bold uppercase">{TTitles()[0]}</h3>
         </div>
         <div className="h-full flex flex-col items-center justify-between sm:gap-4">
             <img src={studioOverview} className={`h-[400px] w-[400px] aspect-square object-contain select-none transition-opacity duration-500 ${animateTrigger ? 'opacity-100' : 'opacity-0'}`} alt="Yoga Studio Gallery Images" />
-            <h3 className="cinzel text-2xl font-bold uppercase">Studio</h3>
+            <h3 className="cinzel text-2xl font-bold uppercase">{TTitles()[1]}</h3>
         </div>
         <div className="h-full flex flex-col items-center justify-between sm:gap-4">
             <img src={yogaOverview} className={`h-[400px] w-[400px] aspect-square object-contain select-none transition-opacity duration-500 ${animateTrigger ? 'opacity-100' : 'opacity-0'}`} alt="Yoga Pose Gallery Images" />
-            <h3 className="cinzel text-2xl font-bold uppercase">Yoga</h3>
+            <h3 className="cinzel text-2xl font-bold uppercase">{TTitles()[2]}</h3>
         </div>
     </section>
   )
--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -1,23 +1,31 @@
-import { yogaTypes } from '../utils';
 import PropTypes from 'prop-types';
+
+import YT1 from "../assets/imgs/icons/yoga-type-1.webp";
+import YT2 from "../assets/imgs/icons/yoga-type-2.webp";
+import YT3 from "../assets/imgs/icons/yoga-type-3.webp";
+import YT4 from "../assets/imgs/icons/yoga-type-4.webp";
 
 Session.propTypes = {
     type: PropTypes.oneOf([0, 1, 2, 3, 4]),
+    instructor: PropTypes.string,
     name: PropTypes.string,
     start: PropTypes.string,
     end: PropTypes.string,
-    desc: PropTypes.string
+    desc: PropTypes.string,
+    alt: PropTypes.string
 };
 
-export default function Session({type=1, start="00:00", end="00:00", desc=null}) {
+export default function Session({type=1, instructor, start="00:00", end="00:00", desc=null, alt=""}) {
+const imgs = [YT1, YT2, YT3, YT4];
+
   return (
-    <td title={type == 0 ? '':`The ${yogaTypes[type-1].name} Yoga Course start at '${start}', and end at '${end}' in (24h format)`} className='h-[80px]'>
+    <td title={type == 0 ? '': instructor} className='h-[80px]'>
       <div className={`h-[80px] relative ${type == 0 ? 'session-free' : `session session-type-${type}`}`}>
         {
           type == 0 ? <></> : 
           <>
           { desc && <h6 style={{color: `var(--yoga-drk-type-${type})`}} className='cinzel w-full h-full z-30 absolute top-2 left-1/2 -translate-x-1/2 text-center uppercase font-bold'>{desc}</h6> }
-          <img src={yogaTypes[type-1].img} className="h-full object-cover object-center aspect-square" alt={`${yogaTypes[type-1].name} Yoga Course`}/>
+          <img src={imgs[type-1]} className="h-full object-cover object-center aspect-square" alt={alt}/>
           <h6 style={{color: `var(--yoga-drk-type-${type})`}} className={`cinzel w-full h-full z-30 absolute top-1/2 -translate-y-1/2 translate-x-1/2 -left-1/2 flex justify-around items-center sm:text-xl text-lg sm:flex-row font-bold`}>
             {start && <span style={{color: `var(--yoga-drk-type-${type})`}}>{start}</span>}
             {end && <span style={{color: `var(--yoga-drk-type-${type})`}}>{end}</span>}
