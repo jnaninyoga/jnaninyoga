71fcdbac8e6b3be7dfe3d99c992779f77a194450
vvhybe <vvhybe@hotmail.com>
2023-07-14 17:58:31 +0100

    implement hydration for react sanp

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import Router from './router'
-import Suspens from './layouts/Suspens';
+import Core from './core';
 import "handyscript";
 // I18NEXT - Localization
 import './locale/index'
 // TAILWIND - CSS
 import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Suspense fallback={<Suspens/>}>
-    <React.StrictMode>
-      <Router />
-    </React.StrictMode>
-  </Suspense>
-)
+const rootElement = document.getElementById("root");
+// const reactRoot = ReactDOM.createRoot(rootElement);
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(rootElement, <Core />);
+} else {
+  ReactDOM.createRoot(rootElement).render(<Core />);
+}
