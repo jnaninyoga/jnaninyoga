db8f0a442e58b2c2e79ea0a40502c55f2b16504a
vvhybe <vvhybe@hotmail.com>
2023-07-16 01:25:59 +0100

    dump react-helmet-async

--- src/core/index.jsx
+++ src/core/index.jsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from 'react'
 import Router from '../router'
 import Suspens from '../layouts/Suspens';
+// import { HelmetProvider } from 'react-helmet-async';
 
 export default function Core() {
     return (
         <React.StrictMode>
             <Suspense fallback={<Suspens/>}>
-              <Router />
+                <Router />
             </Suspense>
         </React.StrictMode>
     )
