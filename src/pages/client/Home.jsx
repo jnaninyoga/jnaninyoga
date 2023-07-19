9c460dfc17b6c91e40970ed2a3cdf6fa7cd9c83f
vvhybe <vvhybe@hotmail.com>
2023-07-19 12:23:49 +0100

    adding reviews

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -10,6 +10,7 @@ import Meta from "../../meta";
 import metadata from "../../meta/meta";
 import { useActivePage, usePathLanguage } from "../../hooks";
 import NotFound from "../../layouts/404";
+import Reviews from "../../layouts/Reviews";
 // import { useParams } from "react-router-dom/dist";
 // import OGP from '../constant/ogp';
 
@@ -27,11 +28,14 @@ export default function Home() {
     <>
       <Meta title={t('home.meta.title')} {...metadata.home}/>
       <Header/>
-      <Hero/>
-      <Overview/>
-      <Yoga/>
-      <ContactCard/>
-      <AboutMe/>
+      <section className="w-screen flex flex-1 justify-center items-center flex-col overflow-hidden">
+        <Hero/>
+        <Overview/>
+        <Yoga/>
+        <ContactCard/>
+        <AboutMe/>
+        <Reviews/>
+      </section>
       <Footer/>
     </>
   )
