328f3703c277eb49e2cbdae962beedbcb2057282
vvhybe <vvhybe@hotmail.com>
2023-07-19 13:25:00 +0100

    make the map bigger in the mobile version

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -17,7 +17,7 @@ export default function Map({title}){
             <img src={icon} className={`${isMapIntersected ? "scale-1" : "scale-0"} h-12 w-12 transition-all duration-500 select-none`} alt="Red/pink lotus icon" />
             <h2 className={`${isMapIntersected ? "translate-y-0 opacity-100" : "translate-y-[100%] opacity-0"} cinzel sm:text-4xl text-2xl font-bold uppercase text-center transition-all duration-500`}>{title}</h2>
             <iframe
-            className={`${isMapIntersected ? "opacity-100" : "opacity-0"} border-none w-full sm:max-h-[500px] max-h-[500px] object-cover object-center aspect-video transition-all duration-700`}
+            className={`${isMapIntersected ? "opacity-100" : "opacity-0"} border-none w-full sm:max-h-[500px] sm:h-[500px] h-[400px] max-h-[600px] object-cover object-center aspect-video transition-all duration-700`}
             title={title}
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13586.97703150534!2d-7.999672!3d31.6408519!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdafef3ed7d51c8b%3A0x1e8998c3ca75bf31!2sJNANIN%20Yoga%20Studio!5e0!3m2!1sen!2sma!4v1689438289984!5m2!1sen!2sma"
             allowFullScreen={true}
