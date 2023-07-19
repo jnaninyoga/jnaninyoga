5717ec3ede24e6302330b969cba2a42dc6d70e2a
vvhybe <vvhybe@hotmail.com>
2023-07-19 12:17:35 +0100

    updating the for layout

--- src/layouts/Form.jsx
+++ src/layouts/Form.jsx
@@ -3,7 +3,6 @@ import { useState, useRef, useEffect } from 'react';
 import { useIntersectView } from '../hooks';
 import icon from '../assets/imgs/icons/lotus.webp';
 import PropsTypes from 'prop-types';
-import Followers from '../components/Followers';
 // import { formFields } from '../utils';
 
 Form.propTypes = {
@@ -13,10 +12,12 @@ Form.propTypes = {
     sendBtn: PropsTypes.string,
     resetBtn: PropsTypes.string,
     onEmpty: PropsTypes.string,
+    insertElement: PropsTypes.element,
     onSubmit: PropsTypes.func,
+    onReset: PropsTypes.func,
 }
 
-export default function Form({title, state, fields, sendBtn, resetBtn, onEmpty, onSubmit=e=>e.preventDefault()}) {
+export default function Form({title, state, fields, sendBtn, resetBtn, onEmpty, insertElement, onSubmit=e=>e.preventDefault(), onReset=e=>e.preventDefault()}) {
     const [form, setForm] = state;
     const [emptyError, setEmptyError] = useState({});
     const [error, setError] = useState({});
@@ -55,9 +56,6 @@ export default function Form({title, state, fields, sendBtn, resetBtn, onEmpty, 
         const empty = Object.values(emptyError).filter(value => value != "");
         const errors = Object.values(error).filter(value => value != "");
 
-        console.log({formdata, empty, errors});
-        console.log({form, emptyError, error});
-
         setIsEmptyError(formdata.length < fields.length || empty.length > 0);
         if(!isEmptyError && errors.length === 0 && formdata.length === fields.length ){
             // calling the onSubmit prop
@@ -79,12 +77,15 @@ export default function Form({title, state, fields, sendBtn, resetBtn, onEmpty, 
   return (
     <form ref={formRef} action="" onSubmit={submitForm} className='sm:w-[50vw] w-full sm:px-20 px-2 flex items-center gap-1 flex-col'>
         <img src={icon} className={`${isFormIntersected ? "scale-1" : "scale-0"} sm:h-16 sm:w-16 h-12 w-12 transition-all duration-500 select-none`} alt="Red/pink lotus icon" />
-        <h1 className={`${isFormIntersected ? "translate-y-0 opacity-100" : 'translate-y-[100%] opacity-0'} cinzel text-yoga-white sm:text-3xl text-2xl font-bold uppercase transition-all duration-500`}>{title}</h1>
+        <h1 className={`${isFormIntersected ? "translate-y-0 opacity-100" : 'translate-y-[100%] opacity-0'} cinzel text-yoga-white text-center sm:text-3xl text-2xl font-bold uppercase transition-all duration-500`}>{title}</h1>
+        {insertElement && <div className={`${isFormIntersected ? "translate-y-0 opacity-100" : 'translate-y-[100%] opacity-0'} my-4 transition-all duration-500 delay-200`}>
+            {insertElement}
+        </div>}
         <p className={`${isFormIntersected && isEmptyError ? "translate-y-0 opacity-100 mb-2" : 'translate-y-[100%] opacity-0 mb-0'} text-center sm:text-lg text-base font-bold form-label-error transition-all duration-500`}>{onEmpty}</p>
         <div ref={wrapper} className='w-full flex items-center justify-center gap-4 flex-col'>
             {fields.map((field, index) => 
             field.type === 'textarea' ?
-            <div key={index} className='w-full h-fit flex flex-col gap-1'>
+            <div key={index} className='relative w-full h-fit flex flex-col gap-1'>
             <textarea
                 onChange={setFormField}
                 // expand the textarea as the user types new lines
@@ -93,6 +94,10 @@ export default function Form({title, state, fields, sendBtn, resetBtn, onEmpty, 
                 name={field.name}
                 placeholder={field.placeholder.toLowerCase().toCapitalCase()}
                 />
+                {// check if the textarea have a maxChars rule like maxChars = 500
+                (field.maxChars && form[field.name]?.length > 0) &&
+                <p className={`z-30 absolute bottom-2 ltr:right-3 rtl:left-3 p-0 m-0 text-sm transition-all duration-[${ 100 * index + 100 }ms]`}>{form[field.name].length}/{field.maxChars}</p>}
+
                 {emptyError[field.name] ? <p className={`${isWrapperIntersected ? "translate-y-0 opacity-100": 'translate-y-[100%] opacity-0'} pl-2 m-0 font-bold text-sm form-label-error transition-all duration-[${ 100 * index + 100 }ms]`}>{emptyError[field.name]}</p>
                 : error[field.name] ? <p className={`${isWrapperIntersected ? "translate-y-0 opacity-100": 'translate-y-[100%] opacity-0'} pl-2 m-0 font-bold text-sm form-label-error transition-all duration-[${ 100 * index + 100 }ms]`}>{error[field.name]}</p>
                 :<></>}
@@ -113,11 +118,10 @@ export default function Form({title, state, fields, sendBtn, resetBtn, onEmpty, 
             </div>
             )}
         </div>
-        <div dir='ltr' ref={btnsRef} className='w-full my-4 flex justify-around items-center'>
-            <button type="reset" className={`${isBtnsIntersected ? "translate-x-0 opacity-100" : 'translate-x-[-100%] opacity-0'} yoga-btn-sec hover:yoga-btn`}>{resetBtn}</button>
-            <button type="submit" className={`${isBtnsIntersected ? "translate-x-0 opacity-100" : 'translate-x-[100%] opacity-0'} yoga-btn`}>{sendBtn}</button>
+        <div dir='ltr' ref={btnsRef} className='w-full my-4 flex justify-around items-center overflow-hidden'>
+            {resetBtn && <button type="reset" onClick={onReset} className={`${isBtnsIntersected ? "translate-x-0 opacity-100" : 'translate-x-[-100%] opacity-0'} yoga-btn-sec hover:yoga-btn`}>{resetBtn}</button> }
+            <button type="submit" className={`${resetBtn ? `${isBtnsIntersected ? "translate-x-0 opacity-100" : 'translate-x-[100%] opacity-0'}` : `${isBtnsIntersected ? "translate-y-0 opacity-100" : 'translate-y-[100%] opacity-0'}`} ${!resetBtn ? "w-[65%]" : ''} yoga-btn`}>{sendBtn}</button>
         </div>
-        <Followers/>
     </form>
   )
 }
