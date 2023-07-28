eb232a2372df8914c278aaee3533f50899e2a544
vvhybe <vvhybe@hotmail.com>
2023-07-28 14:20:09 +0100

    Classes Board - Not implemented yet

---
+++ src/pages/admin/Classes.jsx
@@ -0,0 +1,9 @@
+import NotImplemented from '../../layouts/NotImplemented'
+
+export default function Classes() {
+  return (
+    <div className='p-28 h-full'>
+      <NotImplemented />
+    </div>
+  )
+}
