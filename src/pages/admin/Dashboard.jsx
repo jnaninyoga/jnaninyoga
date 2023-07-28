b9d779ede77d357b5400ea46a8e0286a2e1d75c8
vvhybe <vvhybe@hotmail.com>
2023-07-28 14:16:55 +0100

    Toggel between boards

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import { useAdminAuth } from "../../hooks"
+import ActiveBoardProvider from "../../context/activeboard";
+import DashboardProvider from "../../context/dashboard";
 import DashboardSidebar from "../../layouts/DashboardSidebar";
+import Switchboard from "./Switchboard";
 
 export default function Dashboard() {
-    useAdminAuth();
-    const [activeBoard, setActiveBoard] = useState("contacts");
-
   return (
     <main className="flex w-screen lg:flex-row flex-col">
-      <DashboardSidebar board={[activeBoard, setActiveBoard]}/>
-      <section>
-        
-      </section>
+        <ActiveBoardProvider>
+          <DashboardProvider>
+              <DashboardSidebar/>
+              <Switchboard />
+          </DashboardProvider>
+        </ActiveBoardProvider>    
     </main>
   )
 }
