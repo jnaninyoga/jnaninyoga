e6bc319bffb43fa113de8bf369bb9d80a319fa4d
vvhybe <vvhybe@hotmail.com>
2023-07-28 14:11:41 +0100

    animated icons

--- src/layouts/Suspens.jsx
+++ src/layouts/Suspens.jsx
@@ -1,10 +1,16 @@
+import Icon from "../assets/svg"
 import OverLaped from "./OverLaped"
 
 export default function Suspens() {
   return (
     <OverLaped>
-      <section className="w-[90vw] flex flex-1 items-center justify-center">
-        <h1 className="cinzel p-20 sm:text-8xl text-4xl text-yoga-white animate-pulse">Loading...</h1>
+      <section className="w-[90vw] flex flex-1 items-center justify-center flex-col">
+        <Icon
+          label="Lotus"
+          src="https://cdn.lordicon.com/hvuelaml.json"
+          colors={{oc: "#ffffff", pc: "#fdc5ba"}}
+        />
+        <h1 className="cinzel sm:text-8xl text-4xl text-yoga-white animate-pulse">Loading...</h1>
       </section>
     </OverLaped>
   )
--- src/layouts/Thank.jsx
+++ src/layouts/Thank.jsx
@@ -4,6 +4,7 @@ import banner from  "../assets/imgs/stock/thankpage.webp"
 import { useTranslation } from "react-i18next";
 import { usePathLanguage } from "../hooks";
 import PropTypes from 'prop-types';
+import Icon from "../assets/svg";
 
 Thank.propTypes = {
     mr: PropTypes.string,
@@ -17,9 +18,16 @@ export default function Thank({mr, onClick}) {
   return (
     <>
     <OverLaped banner={banner}>
-        <section className="sm:w-[90vw] flex flex-1 justify-center items-center flex-col gap-4">
+        <section className="sm:w-[90vw] h-full flex flex-1 justify-center items-center flex-col gap-4">
+            <div className="flex flex-col justify-center items-center">
+              <Icon
+              label="Lotus"
+              src="https://cdn.lordicon.com/hvuelaml.json"
+              colors={{oc: "#ffffff", pc: "#fdc5ba"}}
+              />
+              <h1 className="cinzel sm:text-4xl text-4xl text-center text-yoga-white font-bold uppercase">{t("thankpage.title", {mr})}</h1>
+            </div>
             <article className="w-full sm:p flex flex-1 justify-center items-center flex-col sm:gap-6 gap-4">
-              <h1 className="cinzel sm:text-4xl text-3x text-center text-yoga-white font-bold uppercase">{t("thankpage.title", {mr})}</h1>
               <p className="cinzel sm:text-2xl text-center text-yoga-white font-bold">{t("thankpage.text")}</p>
               <button onClick={onClick} className="cinzel sm:text-2xl text-xl font-bold yoga-btn">{t("thankpage.btn")}</button>
             </article>
