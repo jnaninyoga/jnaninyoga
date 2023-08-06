5748ad21018a66ef6bbd56ed28b0434a9add158a
vvhybe <vvhybe@hotmail.com>
2023-08-06 23:38:26 +0100

    update the router and adding error bounderies

--- src/layouts/Error.jsx
+++ src/layouts/Error.jsx
@@ -1,29 +1,34 @@
+import { useRouteError } from "react-router-dom";
 import Icon from "../assets/svg"
 import OverLaped from "./OverLaped"
 import PropTypes from 'prop-types';
 
 Error.propTypes = {
     error: PropTypes.string,
+    icon: PropTypes.string,
+    title: PropTypes.string,
+    btn: PropTypes.string,
 };
 
-export default function Error({error}) {
+export default function Error({error, title="🛑 Something Went Wrong 🛑!", btn="Try Again", icon="https://cdn.lordicon.com/vyukcgvf.json"}) {
+    const routerError = useRouteError();
+
     // refresh the page
     const refreshPage = () => {
         window.location.reload();
     }
-
 
   return (
     <OverLaped>
       <section className="w-[90vw] flex flex-1 items-center justify-center flex-col">
         <Icon
           label="Lotus"
-          src="https://cdn.lordicon.com/hvuelaml.json"
-          colors={{oc: "#ffffff", pc: "#fdc5ba"}}
+          src={icon}
+          colors={{oc: "#ffffff", pc: "#fdc5ba", sc: "#8CC9D2"}}
         />
-        <h1 className="cinzel text-center sm:text-8xl text-4xl text-yoga-white animate-pulse">🛑 Something Went Wrong 🛑!</h1>
-        {error && <p className="text-yoga-white mt-2">{error}</p>}
-        <button onClick={refreshPage} className="yoga-btn mt-4">Try Again</button>
+        <h1 className="cinzel text-center sm:text-8xl text-4xl text-yoga-white animate-pulse">{title}</h1>
+        {(error || routerError) && <p className="text-yoga-white mt-2">{error || routerError}</p>}
+        <button onClick={refreshPage} className="yoga-btn mt-4">{btn}</button>
       </section>
     </OverLaped>
   )
