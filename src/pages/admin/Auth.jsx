167ebbc9c15bfca7cb57cff2da1563f44ee5f4d6
vvhybe <vvhybe@hotmail.com>
2023-08-21 21:44:24 +0100

    update error handelling in the admin login form

--- src/pages/admin/Auth.jsx
+++ src/pages/admin/Auth.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import OverLaped from "../../layouts/OverLaped";
 import Footer from "../../layouts/Footer";
 import Form from "../../layouts/Form";
-import { adminLoginFields, tokenCoder } from "../../utils";
+import { tokenCoder } from "../../utils";
+import { adminLoginFields } from "../../utils/form";
 import LotusOverlay from "../../assets/imgs/icons/lotusOverlay.webp";
 import { useTranslation } from "react-i18next";
 import { docSnap } from "../../firebase";
@@ -22,35 +23,43 @@ export default function Auth() {
 
     const [auth, setAuth] = useState({});
     // error messages for the login form
-    const [error, setError] = useState();
+    const [error, setError] = useState(false);
 
     const TFields = t('adminauth.form.fields', {returnObjects: true});
     const TFieldsErrors = t('adminauth.form.fieldserrors', {returnObjects: true});
   
-    adminLoginFields.forEach((field) => {
+    const AdminLoginFields = useMemo(() => adminLoginFields.map(field => {
       field.placeholder = TFields[field.name.toLowerCase()] || field.placeholder;
       field.error = TFieldsErrors[field.name.toLowerCase()] || field.error;
-      field.emptyError = t('contact.form.empty', {field: field.placeholder});
-    });
+      field.empty = t('contact.form.empty', {field: field.placeholder});
+      return field;
+    }), [TFields, TFieldsErrors, t]);
+
+
     
     const validateAuth = async (authdata) => {
       try{
+        // clear the error message
+        setError(false);
+        // get the admin data from the database
         const admin = await (await docSnap(collections.auth)).docs[0].data();
         if (admin.username === authdata.username && admin.password === authdata.password){
           tokenCoder("yogacoach", authdata);
-          navigate(`/lotus/${currentLanguage.code}`);
+          navigate(`/lotus`);
         } else {
-          setError(t('adminauth.form.onEmpty'));
+          setError(true);
         }
       }catch(error){
-        console.log(error);
+        console.error(error);
       }
     }
 
-    /// 5s timeout to clear the error message
+    // effect to clear custom error messages
     useEffect(() => {
-      if(!error) return;
-      const timeout = setTimeout(() => setError(), 5000);
+      if(!error) return
+      const timeout = setTimeout(() => {
+        setError(false);
+      }, 5000);
       return () => clearTimeout(timeout);
     }, [error]);
 
@@ -59,13 +68,15 @@ export default function Auth() {
     <OverLaped>
         <img src={LotusOverlay} className={`opacity-100 -z-10 absolute scale-75 sm:bottom-6 bottom-0 sm:right-4 right-1 object-cover object-center mix-blend-screen transition-all duration-700 delay-300`} alt="Lotus Overlay" />
         <Form
-        onSubmit={validateAuth}
-        onEmpty={t('adminauth.form.onEmpty')}
-        onError={error}
+        animatedIcon
         title={t('adminauth.title')}
         state={[auth, setAuth]}
-        fields={adminLoginFields}
-        sendBtn={t('adminauth.form.login')}
+        fields={AdminLoginFields}
+        onSubmit={validateAuth}
+        submitBtn={t('adminauth.form.login')}
+        EmptyErrorMessage={t('adminauth.form.error')}
+        ErrorMessage={t('adminauth.form.error')}
+        errorTrigger={error}
         />
     </OverLaped>
     <Footer/>
