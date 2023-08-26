1891f1767cba1edfa2bec06dc554d004bdf55948
vvhybe <vvhybe@hotmail.com>
2023-08-26 13:59:30 +0100

    adding users and carnets board

--- src/pages/admin/Switchboard.jsx
+++ src/pages/admin/Switchboard.jsx
@@ -1,33 +1,47 @@
+import { Helmet } from "react-helmet-async";
 import { useActiveBoard } from "../../hooks"
-import { dashboardNavicons } from "../../utils";
+import { dashboardNavbar } from "../../utils";
 // Board Components
 import Account from "./Account";
 import Books from "./Books";
-import Classes from "./Classes";
+import Carnets from "./Carnets";
+import Sessions from "./Sessions";
 import Contacts from "./Contacts";
 import Reviews from "./Reviews";
+import Users from "./Users";
+import { useMemo } from "react";
 
 export default function Switchboard() {
     const { activeBoard } = useActiveBoard();
-    
+
+    // get the icon for the active board
+    const ActiveBoardIcon = useMemo(() => dashboardNavbar.find(board => board.name.toLowerCase() === activeBoard.toLowerCase()).icon, [activeBoard]);
+
     // switch between the different boards
     const Board = () => {
         switch (activeBoard?.toLowerCase()) {
             case "account": return <Account />;
-            case "classes": return <Classes />;
+            case "sessions": return <Sessions />;
             case "contacts": return <Contacts />;
             case "books": return <Books />;
             case "reviews": return <Reviews />;
-            default: return <Contacts />;
+            case "users": return <Users />;
+            case "carnets": return <Carnets />;
+            default: return <Users />;
         }
     }
 
     return (
-        <section className={`relative container w-full h-screen flex items-center flex-col ${"overflow-x-hidden overflow-y-auto"}`}>
-            <header className="w-full p-4 flex justify-center items-start flex-col sm:gap-4 gap-2">
-                <h2 className="cinzel w-full flex justify-center items-center gap-4 sm:text-2xl text-xl text-yoga-green-dark font-bold uppercase py-4 px-6 outline outline-[3px] -outline-offset-[10px] bg-yoga-red outline-white transition-all duration-300"><i className={`flex justify-center items-center text-yoga-green-dark ${dashboardNavicons[activeBoard]}`}></i> {activeBoard}</h2>
-            </header>
+        <>
+        <Helmet>
+            <title>{activeBoard.toCapitalCase()} Dashboard - Jnanin Yoga Studio</title>
+        </Helmet>
+        <section className={`relative container w-full h-screen flex items-center flex-col overflow-x-hidden overflow-y-auto`}>
+            { (activeBoard?.toLowerCase() !== "sessions") && <header className="w-full p-4 flex justify-center items-start flex-col sm:gap-4 gap-2">
+                <h2 className="cinzel w-full flex justify-center items-center gap-4 sm:text-2xl text-xl text-yoga-green-dark font-bold uppercase py-4 px-6 outline outline-[3px] -outline-offset-[10px] bg-yoga-red outline-white transition-all duration-300"><i className={`flex justify-center items-center text-yoga-green-dark ${ActiveBoardIcon}`}></i> {activeBoard}</h2>
+            </header> }
             <Board />
         </section>
+        </>
     );
 }
