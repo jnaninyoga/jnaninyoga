1a104e5d46dff714b1e1e17631153a3bf1896710
vvhybe <vvhybe@hotmail.com>
2023-08-27 13:58:40 +0100

    update gallery images

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,24 +7,21 @@ import C1 from "../assets/imgs/stock/classes-1.webp";
 import C2 from "../assets/imgs/stock/classes-2.webp";
 import C3 from "../assets/imgs/stock/classes-3.webp";
 import C4 from "../assets/imgs/stock/classes-4.webp";
-import C5 from "../assets/imgs/stock/classes-5.webp";
 // Studio - Stock Images
 import S1 from "../assets/imgs/stock/studio-1.webp";
 import S2 from "../assets/imgs/stock/studio-2.webp";
 import S3 from "../assets/imgs/stock/studio-3.webp";
 import S4 from "../assets/imgs/stock/studio-4.webp";
-import S5 from "../assets/imgs/stock/studio-5.webp";
 // Yoga - Stock Images
 import Y1 from "../assets/imgs/stock/yoga-1.webp";
 import Y2 from "../assets/imgs/stock/yoga-2.webp";
 import Y3 from "../assets/imgs/stock/yoga-3.webp";
 import Y4 from "../assets/imgs/stock/yoga-4.webp";
-import Y5 from "../assets/imgs/stock/yoga-5.webp";
 
 export const OverviewStockImgs = {
-    classes: [C1, C2, C3, C4, C5],
-    studio: [S1, S2, S3, S4, S5],
-    yoga: [Y1, Y2, Y3, Y4, Y5]
+    classes: [C1, C2, C3, C4],
+    studio: [S1, S2, S3, S4],
+    yoga: [Y1, Y2, Y3, Y4]
 };
 
 // Yoga Calander:
