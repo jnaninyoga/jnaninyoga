d48f361fb98c55145fbb148ecf60718f88f1b643
vvhybe <vvhybe@hotmail.com>
2023-09-16 22:46:01 +0100

    change Hero image with motivation video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import HeroBG from '../assets/imgs/stock/yogapose.webp';
+import HeroBanner from '../assets/videos/yogahero.mp4';
 import Card from '../layouts/Card';
 import { useTranslation } from 'react-i18next';
 import { useActivePage } from '../hooks';
@@ -10,7 +10,8 @@ export default function Hero() {
 
   return (
     <section className='relative flex flex-1 items-center justify-center flex-col'>
-        <img className='trany w-screen h-full object-cover object-center select-none' src={HeroBG} alt="Yoga Position" />
+        {/* <img className='trany w-screen h-full object-cover object-center select-none' src={HeroBG} alt="Yoga Position" /> */}
+        <video role="banner" poster={"/assets/gallery/2.webp"} onContextMenu={e => e.preventDefault()} src={HeroBanner} muted loop autoPlay preload='auto' className='w-screen h-[90vh] object-cover object-center pointer-events-none select-none'></video>
         <Card 
         twStyle={'sm:-translate-y-28 -translate-y-14'} 
         title={t(`${activePage}.hero.title`)}
